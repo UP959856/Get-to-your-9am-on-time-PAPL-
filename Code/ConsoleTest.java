000651dc26bbdda7142e6dff06e27913adc57d2c
UP959856 <72748038+UP959856@users.noreply.github.com>
2022-01-19 16:31:23 +0000

    Create ConsoleTest.java

---
+++ Code/ConsoleTest.java
@@ -0,0 +1,14 @@
+package Code;
+
+import java.io.FileNotFoundException;
+
+public class ConsoleTest {
+public static void main(String[] args) throws FileNotFoundException {
+
+    NodeMap map = new NodeMap();
+
+    Console c = new Console(map);
+
+}
+    
+}
