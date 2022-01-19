27a19728ec90202cd67942a2bb864511d249ce2a
UP959856 <72748038+UP959856@users.noreply.github.com>
2022-01-19 15:43:38 +0000

    Create TestdbFile.java

---
+++ Code/TestdbFile.java
@@ -0,0 +1,22 @@
+package Code;
+
+import java.io.FileNotFoundException;
+
+public class TestdbFile {
+
+static NodeCollection nodeCollection;
+public static void main(String[] args) throws FileNotFoundException {
+    try {
+        nodeCollection = new NodeCollection();
+    } catch (
+            FileNotFoundException e) {
+         print(e.getMessage());
+        return;
+    }
+
+    print(nodeCollection.toString());
+}
+
+public static void print(String  info){System.out.println(info);}
+    
+}
