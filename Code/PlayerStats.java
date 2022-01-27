2289697bbff23385566b1c18161508c3e0eb9d21
UP959856 <72748038+UP959856@users.noreply.github.com>
2022-01-27 22:30:14 +0000

    Update PlayerStats.java

--- Code/PlayerStats.java
+++ Code/PlayerStats.java
@@ -1,26 +1,29 @@
 public class PlayerStats {
 
-    public int coolness;
-    public int time;
+    public float coolness;
+    public float time;
 
-    public void playerStats(int coolness, int time){
+    public void playerStats(float coolness, float time){
 
         this.coolness = coolness;
         this.time = time;
 
     }
 
-    //Getters, setters and adders for points.
+//Getters, setters and adders for points.
 
-    public int getCoolnessPoints(){return coolness;}
-    public void setCoolnessPoints(int coolness){this.coolness = coolness;}
-    public void addCoolnessPoints(int points){this.coolness = this.coolness + points;}
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public float getCoolnessPoints(){return coolness;}
+    public void setCoolnessPoints(float coolness){this.coolness = coolness;}
+    public void addCoolnessPoints(float points){this.coolness = this.coolness + points;}
 
-    public int getTimePoints(){return time;}
-    public void setTimePoints(int time){this.time = time;}
-    public void addTimePoints(int points){this.time = this.time + time;}
+    public float getTimePoints(){return time;}
+    public void setTimePoints(float time){this.time = time;}
+    public void addTimePoints(float points){this.time = this.time + time;}
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    public boolean hasStats(int coolness, int time, int requiredCoolness, int requiredTime){
+//Method to check if player has required stats to travel to a node with requirements.
+    public boolean hasStats(float coolness, float time, float requiredCoolness, float requiredTime){
 
         boolean hasRequiredStats;
 
@@ -31,6 +34,12 @@ public class PlayerStats {
 
         return hasRequiredStats;
         
+    }
+
+    public String toString(){
+
+        return "Coolness: " + coolness + "\n" + "Time: " + time;
+
     }
     
 }
