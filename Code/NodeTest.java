dc364f26f54d8da204700fbba7126fce00aa1f77
UP959856 <72748038+UP959856@users.noreply.github.com>
2022-01-27 18:47:58 +0000

    Update NodeTest.java

--- Code/NodeTest.java
+++ Code/NodeTest.java
@@ -6,7 +6,7 @@ public static void main(String[] args){
 
     String line0 = "1,2,4, You awaken from a strange dream and you look at your phone and realize the time is 8.30am. It is a bleak grey monday morning and you have a 9am. You think to yourself 'I better get to my 9am on time.' as a wave of anxiety hits., Get up and ready to go, Pursue the Dream";
     String line1 = "2,3,5, You are by the front door., Smoke cigarette in backyard, Head outside";
-    String line2 = "4,0,0, (Ending 1) You fall asleep, Restart";
+    String line2 = "4,0,0, (Ending 1) You fall asleep, Restart,-";
 
     Node n0 = new Node();
     mapNode(n0, line0);
